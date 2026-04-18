a07c51309b502acc7022bc1214698574ee1cb9d3
Patrik Csak <p@trikcsak.com>
2026-04-17 19:19:05 -0700

    fix eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -2,15 +2,27 @@ import {defineConfig} from 'eslint/config';
 import packageJson from 'eslint-plugin-package-json';
 import xo from 'xo';
 
-export default defineConfig([
-	packageJson.configs.recommended,
-	packageJson.configs.stylistic,
-	...xo.xoToEslintConfig([
-		{
-			prettier: 'compat',
-			rules: {
-				'no-await-in-loop': 'off',
-			},
+export default defineConfig(
+	{
+		files: ['**/*.js'],
+		extends: [
+			xo.xoToEslintConfig([
+				{
+					prettier: 'compat',
+					rules: {
+						'no-await-in-loop': 'off',
+					},
+				},
+			]),
+		],
+	},
+	{
+		files: ['package.json'],
+		extends: [packageJson.configs.recommended, packageJson.configs.stylistic],
+		rules: {
+			'package-json/require-exports': 'off',
+			'package-json/require-sideEffects': 'off',
+			'package-json/scripts-name-casing': 'off',
 		},
-	]),
-]);
+	},
+);
